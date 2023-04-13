702b88031a6c1a671450d85fa98f7f5394d4e210
Jeong-JaeHoon1 <ne4355@gmail.com>
2023-04-14 01:36:44 +0900

    test

--- Semi_Project/src/com/saltmarket/board/controller/hi.java
+++ Semi_Project/src/com/saltmarket/board/controller/hi.java
@@ -3,5 +3,8 @@ package com.saltmarket.board.controller;
 public class hi {
 	public static void main(String[] args) {
 		System.out.println("하이하이 ");
+		
+		System.out.println("재훈이 테스트");
+		
 	}
 }
