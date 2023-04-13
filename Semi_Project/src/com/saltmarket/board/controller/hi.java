28bb7f4170ad1f722c8f105a8f4fbd0bff93d645
TaeSick <sun0571027@gmail.com>
2023-04-14 01:36:22 +0900

    태식이테스트

--- Semi_Project/src/com/saltmarket/board/controller/hi.java
+++ Semi_Project/src/com/saltmarket/board/controller/hi.java
@@ -3,5 +3,13 @@ package com.saltmarket.board.controller;
 public class hi {
 	public static void main(String[] args) {
 		System.out.println("하이하이 ");
+		
+		int a = 10;
+		int b = 25;
+		
+		System.out.println(a+b);
+		for(int i = 0; i <10; i++) {
+			System.out.println(i);
+		}
 	}
 }
